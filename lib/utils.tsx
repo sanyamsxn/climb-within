46a0cc04f035a1f4a740f26ec3a66aa5f117d647
Sanyam Saxena <sanyamsxn1998@gmail.com>
2025-06-25 14:25:09 +0530

    files added 3

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -1,10 +1,25 @@
-export function cn(...inputs: any[]) {
+// export function cn(...inputs: any[]) {
+//   return inputs
+//     .flatMap(input => {
+//       if (typeof input === "string") return input;
+//       if (typeof input === "object" && input !== null) {
+//         return Object.entries(input)
+//           .filter(([_, value]) => Boolean(value))
+//           .map(([key]) => key);
+//       }
+//       return [];
+//     })
+//     .join(" ");
+// }
+
+
+export function cn(...inputs: Array<string | Record<string, boolean> | undefined | null | false>) {
   return inputs
     .flatMap(input => {
       if (typeof input === "string") return input;
       if (typeof input === "object" && input !== null) {
         return Object.entries(input)
-          .filter(([_, value]) => Boolean(value))
+          .filter(([ value]) => Boolean(value))
           .map(([key]) => key);
       }
       return [];
